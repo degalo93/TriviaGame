d53d88f2a4cc2dd34f2e5f848599d70e697f0fef
degalo93 <denisgalo10@gmail.com>
2019-03-01 11:52:49 -0800

    added to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,30 +3,38 @@
 $("#start").on("click", startgame);
 
 
+var Tgame = {
+    correctAnswers = 0,
+    incorrectAnswers = 0,
+    unanswered = 0,
+    time = 30,
+    timerClock = false,
+    clock = " ",
 
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unanswered = 0;
-var time = 30;
-var timerClock = false;
-var clock = " ";
 
-var questions = {
-    question1: "hello",
-    question2: "alright",
-    question3: "ok",
-    question4: "not another q",
-};
-var options = {
-    question1: "hello",
-    question2: "alright",
-    question3: "ok",
-    question4: "cool"
+    questions = {
+        question1: "What is The powerhouse of the cell?",
+        question2: "What is the atomic symbol of silver",
+        question3: "What does ATP stand for",
+        question4: " What is the first element in the periodic table ",
+    },
+    options = {
+        question1: ["ribosome", "DNA", "mitochondria", "hydrogen"],
+        question2: ["aa", "ab", "ga", 'ag'],
+        question3: ["adenosine triphosphate", "Attention triad power", "atacking three pandas", "Association of Tennis Professional"],
+        question4: ["1", " H", "Hi", "He"]
 
-};
-var answers = {
-    question1: "hello",
-    question2: "alright",
-    question3: "ok",
-    question4: "cool"
-};
+    },
+    answers = {
+        question1: "mitochondria ",
+        question2: "ag",
+        question3: "adenosine triphosphate",
+        question4: "H",
+    },
+    startgame: function() {
+        Tgame.correctAnswers = 0;
+        Tgame.incorrectAnswers = 0;
+        Tgame.unanswered = 0;
+        clearInterval(clock);
+    },
+}
